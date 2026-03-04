d4057ee46c698e5f4acdbcc739dcf28003c8c014
Victor-Pasqualotti <147944015+Victor-Pasqualotti@users.noreply.github.com>
2026-03-04 09:12:03 -0300

    Add files via upload

--- exemplo2.py
+++ exemplo2.py
@@ -2,33 +2,51 @@ import concurrent.futures
 import time
 import random
 import pandas as pd
+import threading
 
-df = pd.DataFrame.from_dict({
-    'lista_prompts' : [str(i) for i in range(100)],})
+start_time = time.time()
 
-Batch = 7
-
+# Simula a chamada da IA
 def chama_ia(i, prompts):
-    print(f'Recebi index {i} e lista: {prompts}')
+    print(f'[{threading.current_thread().name}]', f'Recebi index {i} e lista: {prompts}')
+    print('Tempo ate agora: {:.2f} segundos \n ------------------------'.format(time.time() - start_time))
+    time.sleep(random.randint(1, 10))  # Simula o tempo de processamento da IA
     return i, prompts
 
+# Simula publico
+df = pd.DataFrame.from_dict({
+    'primeiro_argumento' : [str(i) for i in range(100)],
+    'segundo_argumento'  : [str(i) for i in range(100,200)],
+})
+#print(df.head())
+
+# Lista de prompts
 lista_prompts = [
-    row['lista_prompts'] for index, row in df.iterrows()
+    f"Argumentos: {row['primeiro_argumento']}, {row['segundo_argumento']}" for index, row in df.iterrows()
 ]
 
-futures = list()
-with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+# Execução em loop, usando Batch de prompts
+Batch = 7
+MaxWorkers = 4
+respostas_futuras = list() # Lista de respostas futuras
+with concurrent.futures.ThreadPoolExecutor(max_workers=MaxWorkers) as executor:
 
+    # A MainThread envia os prompts em lotes para a IA
+    # Faz isso enviando prompts para serem executados em paralelo, em cada trhead.
+    # E armazena a resposta futura (futures) para cada lote de prompts enviado.
     for start in range(0, len(lista_prompts), Batch):
         end = start + Batch
         batch_prompts = lista_prompts[start:end]
-        futures.append(executor.submit(chama_ia, start, batch_prompts))
+        # Envia o lote de prompts para a IA e armazena a resposta futura
+        print('Vou requisitar o processamento do lote: ', start)
+        respostas_futuras.append(executor.submit(chama_ia, start, batch_prompts))
         
-    # 2. A MainThread coleta os resultados à medida que ficam prontos
-    for futuro_concluido in concurrent.futures.as_completed(futures):
-        i, prompts = futuro_concluido.result()
+    # A MainThread coleta os resultados à medida que ficam prontos
+    for resposta_concluida in concurrent.futures.as_completed(respostas_futuras):
+        i, prompts = resposta_concluida.result()
+        print(f'\n[{threading.current_thread().name}]', f'Resposta {i} concluída, processando resultado...')
         end_idx = i + len(prompts) - 1
         df.loc[i:end_idx, 'resposta_ia'] = [p for p in prompts]
 
-print(df.head())
-print(df.tail())
+print(df.head(10))
+print(df.tail(10))
